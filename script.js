22629f7845d42adee5b11fe00b8ccb0bc5b24837
jdog <thuz3rfr13ndly@gmail.com>
2022-04-26 17:09:59 -0600

    add comments

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
 console.log('you clicked the button');
 
+//prompt user for password criteria
+//  1.password length between 8 and 128
+//  2. Lowercase, uppercase, numbers and special characters
+//  3. validate input is correct (true)
+//  4. generate password based on given criteria
+
+
+
+//  5. display password on screen.
 
 
 return "Generator Password goes here dude"; // this will return the generated password
