a1642c833e51c064776b9d84440687f1730f3637
jdog <thuz3rfr13ndly@gmail.com>
2022-04-26 17:03:13 -0600

    comments to pseudocode

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+console.log('you clicked the button');
+
+return "Generator Password goes here dude"; // this will return the generated password
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword();// create function for password generate
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
